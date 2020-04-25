490f7221cb23f0609aecd6d5c669b9ae488f8685
pablobrand <pabrandenburg@gmail.com>
2020-04-25 08:11:41 -0500

    firt commit
    first template setup

---
+++ src/app/navigation-controllers/navigation-controllers.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation-controllers',
+  templateUrl: './navigation-controllers.component.html',
+  styleUrls: ['./navigation-controllers.component.css']
+})
+export class NavigationControllersComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
